d74616b06d5f26fe1d964964d1f70fac59d6fd70
Lionel ORCIL <48251459+ioio2995@users.noreply.github.com>
2025-09-01 10:25:04 +0000

    debug

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,26 @@
 # docs/conf.py
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent / "cosmobewi-shared"))
+
+from conf_shared import *     # shared theme & config
+include_local_paths(__file__) # enable local overrides
+
+# -- Project information -----------------------------------------------------
+project = 'Constante cosmologique et dilution des champs sans masse'
+author = 'Projet Théorie des Horloges Cosmologiques'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
     'sphinxcontrib.bibtex',
 ]
+BASE_PDV = Path(__file__).resolve().parent
+bibtex_bibfiles = [str(BASE_PDV / 'refs.bib')]
 
-bibtex_bibfiles = ['pdv-to-lambda/refs.bib']
+print("[conf] bibtex_bibfiles =", bibtex_bibfiles)
+
+exclude_patterns = []
